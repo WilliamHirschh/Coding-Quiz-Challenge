90e8ae685b031e45fd930d3d0b0106609a1dcc4a
William Hirsch <Williamhirschh@gmail.com>
2022-09-22 13:21:23 -0500

    made basic HTML sctructure

---
+++ index.js
@@ -0,0 +1,56 @@
+var topHeader = docuemnt.querySelector("#topheader");
+var highScores = document.querySelector("#high-scores");
+var time = document.querySelector("#time");
+var displayPage = document.querySelector("#start-display-page")
+var codingParagraph = document.querySelector("#codingQuizChallenge")
+var StartBtn = document.querySelector("#startQuiz");
+
+var questionsPage1 = document.querySelector("#questions");
+var questionsPage2 = document.querySelector(".questions");
+var promptedQuestions = document.querySelector("#promptedQuestion");
+
+var buttons = document.querySelector(".buttonChoice");
+var button1 = document.querySelector("#button1");
+var button2 = document.querySelector("#button2");
+var button3 = document.querySelector("#button3");
+var button4 = document.querySelector("#button4");
+var correcrOrWronf = document.querySelector("#correctOrWrong");
+
+var submittionPage = document.querySelector("#submitScores");
+var finalScore = document.querySelector("#finalScore");
+var submitInitals = document.querySelector("#initials");
+var submitInitialsBtn = document.querySelector("#submitInitialsBtn");
+
+
+var setOfQuestions = [
+
+    {
+        question: "Question 1: String value must be enclosed within _____ when being assigned to variables.",
+        choices: ["a. commas", "b. curly brackets", "c. quotes", "d. parenthesis"],
+        answer: "c"
+    },
+    {
+        question: "Question 2: commonly used data types DONT include:",
+        choices: ["a. strings", "b. booleans", "c. alerts", "d. numbers"],
+        answer: "c"
+    },
+    {
+        question: "Question 3: How do you create a function in JavaScript",
+        choices: ["a. function = myFunction()", "b. function myFunction()", "c. function:myFunction()", "d. createMyFunction()"],
+        answer: "b"
+    },
+    {
+        questi
+    }
+
+
+
+
+
+
+
+
+
+
+
+];
